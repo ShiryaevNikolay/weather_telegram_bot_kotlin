8896cb89dda98ab9131fe5c94c8c50b5ac6ed303
Nikolay Shiryaev <deendeeps@list.ru>
2021-11-21 12:00:34 +0300

    Создается респонс для текущего прогноза погоды

---
+++ src/main/java/service/response/CurrentWeatherResponse.kt
@@ -0,0 +1,66 @@
+package service.response
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Респонс текущего прогноза погоды
+ */
+class CurrentWeatherResponse(
+    @SerializedName("weather")
+    val weather: List<WeatherEntity>,
+    @SerializedName("main")
+    val main: MainEntity,
+    @SerializedName("visibility")
+    val visibility: Int,
+    @SerializedName("wind")
+    val wind: WindEntity,
+    @SerializedName("rain")
+    val rain: RainEntity,
+    @SerializedName("clouds")
+    val clouds: CloudsEntity,
+    @SerializedName("name")
+    val city: String
+)
+
+class WeatherEntity(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("main")
+    val main: String,
+    @SerializedName("description")
+    val description: String,
+    @SerializedName("icon")
+    val icon: String
+)
+
+class MainEntity(
+    @SerializedName("temp")
+    val temp: Float,
+    @SerializedName("feels_like")
+    val feelsLike: Float,
+    @SerializedName("temp_min")
+    val tempMin: Float,
+    @SerializedName("temp_max")
+    val tempMax: Float,
+    @SerializedName("pressure")
+    val pressure: Int,
+    @SerializedName("humidity")
+    val humidity: Int,
+)
+
+class WindEntity(
+    @SerializedName("speed")
+    val speed: Int,
+    @SerializedName("deg")
+    val deg: Int
+)
+
+class RainEntity(
+    @SerializedName("1h")
+    val time: Int
+)
+
+class CloudsEntity(
+    @SerializedName("all")
+    val all: Int
+)
