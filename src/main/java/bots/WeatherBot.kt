98144e73a14a93d348a8680b6735df4f243b2d7e
Nikolay Shiryaev <deendeeps@list.ru>
2021-11-21 15:48:41 +0300

    Реакция на команды /start и /weather

--- src/main/java/bots/WeatherBot.kt
+++ src/main/java/bots/WeatherBot.kt
@@ -5,7 +5,7 @@ import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
 import com.github.kotlintelegrambot.dispatcher.command
 import com.github.kotlintelegrambot.entities.ChatId
-import com.github.kotlintelegrambot.network.fold
+import com.github.kotlintelegrambot.entities.ParseMode
 import domain.CurrentWeather
 import io.reactivex.rxjava3.schedulers.Schedulers
 import service.repository.WeatherRepository
@@ -19,24 +19,58 @@ class WeatherBot(
 
     private val bot: Bot = bot {
         token = TOKEN
+
         dispatch {
+
+            /*
+            Команда запуска бота
+             */
             command("start") {
-                val result = bot.sendMessage(
+                bot.sendMessage(
                     chatId = ChatId.fromId(message.chat.id),
-                    text = "Hello here!"
+                    text = """
+                        Добро пожаловать, ${message.chat.firstName}!
+                        
+                        Я периодически буду присылать прогноз погоды в наш чат :D
+                    """.trimIndent()
                 )
                 getCurrentWeather({ currentWeather ->
-                    println("Попали сюда")
+                    val markdownText = getMessageForCurrentWeather(currentWeather)
+                    val text = """
+                        Опа, а вот и первый прогноз погоды на текущий момент! Можешь не благодарить :Р
+                        
+                        $markdownText
+                    """.trimIndent()
+                    bot.sendMessage(
+                        chatId = ChatId.fromId(message.chat.id),
+                        text = text,
+                        parseMode = ParseMode.MARKDOWN
+                    )
                 }, {
-                    println("Произошла ошибка :(")
+                    bot.sendMessage(
+                        chatId = ChatId.fromId(message.chat.id),
+                        text = "Не удалось получить данные"
+                    )
                 })
-//                result.fold({
-//                    print(data)
-//                    // TODO: сделать что-нибудь с response
-//                }, {
-//                    print(it)
-//                    // TODO: что-то сделать при ошибке
-//                })
+            }
+
+            /*
+            Команда для получения текущего прогноза погоды
+             */
+            command("weather") {
+                getCurrentWeather({ currentWeather ->
+                    val markdownText = getMessageForCurrentWeather(currentWeather)
+                    bot.sendMessage(
+                        chatId = ChatId.fromId(message.chat.id),
+                        text = markdownText,
+                        parseMode = ParseMode.MARKDOWN
+                    )
+                }, {
+                    bot.sendMessage(
+                        chatId = ChatId.fromId(message.chat.id),
+                        text = "Не удалось получить данные"
+                    )
+                })
             }
         }
     }
@@ -45,6 +79,9 @@ class WeatherBot(
         bot.startPolling()
     }
 
+    /**
+     * Получение текущего прогноза погоды
+     */
     private fun getCurrentWeather(
         onSuccess: ((CurrentWeather) -> Unit)? = null,
         onError: ((Throwable) -> Unit)? = null
@@ -53,6 +90,26 @@ class WeatherBot(
             .subscribeOn(Schedulers.io())
             .observeOn(Schedulers.single())
             .subscribe(onSuccess, onError)
+    }
+
+    /**
+     * Возвращает отформатированный тест с прогнозом погоды
+     */
+    private fun getMessageForCurrentWeather(weather: CurrentWeather): String {
+        return """
+            *${weather.city}*
+            Погода: `${weather.weather[0].description}`
+            Температура: `${weather.main.temp.toInt()} C`
+            По ощущениям: `${weather.main.feelsLike.toInt()} C`
+            Влажность: `${weather.main.humidity}%`
+            ${
+                weather.wind?.let { 
+                    "Ветер: `${it.speed} м/с`"       
+                }
+            }
+            Видимость: `${(weather.visibility / 1000)} км`
+            Давление: `${weather.main.pressure} гПа`
+        """.trimIndent()
     }
 
     companion object {
